ec8c34213016a32a0d70fcf9b36bc00b992c7ed2
hajimiHenry <spaner2022@outlook.com>
2025-12-19 13:34:01 +0800

    屎山屎山屎山跑不通的】

--- final-review/4.c
+++ final-review/4.c
@@ -3,7 +3,22 @@
 int mian(void)
 {
 
-
-
+    int N;
+    scanf("%d",&N);
     
+    int p,q;
+    int count[N][q];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d",&p);
+        for (int j = 0; j < p; j++)
+       {
+        scanf("%d",&count[i][j]);
+        /* code */
+       }
+        /* code */
+    }
+    
+
 }
